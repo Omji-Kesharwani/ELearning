0ede3334bc3adf33f7a7aa677faa83559cc32ef6
Omji_Kesharwani <kesharwaniomji000@gmail.com>
2025-06-18 08:31:02 +0530

    commit

--- frontend/src/services/apiconnector.js
+++ frontend/src/services/apiconnector.js
@@ -1,6 +1,12 @@
 import axios from "axios"
 
-export const axiosInstance = axios.create({});
+export const axiosInstance = axios.create({
+    baseURL: "http://localhost:4000/api/v1",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
     return axiosInstance({
